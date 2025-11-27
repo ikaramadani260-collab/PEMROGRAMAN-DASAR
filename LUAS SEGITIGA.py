0d50d11520e342a8e6213df5655aeb8d4fccac39
ikaramadani260-collab <ika.ramadani260@gmail.com>
2025-11-27 09:21:07 +0800

    Create LUAS SEGITIGA.py

---
+++ LUAS SEGITIGA.py
@@ -0,0 +1,10 @@
+# Nama : Ika ramadani
+# NIM : D0425325
+# Luas persegi
+
+s : int(input("Masukkan sisi pertama: "))
+s2 : int(input("Masukkan sisi kedua: "))
+
+luas = int(s * s2)
+
+print('luas persegi adalah: ' , luas)
