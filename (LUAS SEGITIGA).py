83311438f14b91afbee55298a878ba54a5d28ed2
ikaramadani260-collab <ika.ramadani260@gmail.com>
2025-11-27 09:54:26 +0800

    Create (LUAS SEGITIGA).py

---
+++ (LUAS SEGITIGA).py
@@ -0,0 +1,10 @@
+#Nama :Ika Ramadani
+#Nim :D0425325
+#Luas SEGITIGA
+
+alas = int(input("Maksukkan alas segitiga:"))
+tinggi = int(input("Masukkan tinggi segitiga:"))
+
+luas = int((alas*tinggi)/2)
+
+print('luas segitiga adalah:',luas)
