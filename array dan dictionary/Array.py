0792b180ac753f39ac7222cabd5a1a8b6b17f555
ikaramadani260-collab <ika.ramadani260@gmail.com>
2025-12-05 16:26:14 +0800

    Add files via upload

---
+++ array dan dictionary/Array.py
@@ -0,0 +1,16 @@
+# Array
+Mahasiswa = ["aulia", "ika", "asti", "mutia", "dina"]
+
+# Menampilkan semua nama mahasiswa
+print("Daftar Mahasiswa:", Mahasiswa)
+
+# Mengakses 
+print("Mahasiswa pertama:", Mahasiswa[0])
+
+#Menambah data 
+Mahasiswa.append("nadia")
+print("setelah ditambah:", Mahasiswa)
+
+# Menghapus 
+Mahasiswa.remove("mutia")
+print("setelah mutia dihapus:", Mahasiswa)
