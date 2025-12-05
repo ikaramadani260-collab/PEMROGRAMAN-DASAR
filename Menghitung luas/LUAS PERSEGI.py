34dfe5256a59ae16f4cb8af102d523c05c9b13aa
ikaramadani260-collab <ika.ramadani260@gmail.com>
2025-12-05 16:27:54 +0800

    Add files via upload

---
+++ Menghitung luas/LUAS PERSEGI.py
@@ -0,0 +1,11 @@
+# nama : ibrahim
+# nim : d0425013
+# luas persegi
+
+
+s = int(input("masukkan sisi pertama: "))
+s2 = int(input("Masukkan sisi kedua: "))
+
+luas = int(s * s2 ) 
+
+print('luas persegi adalah: ' , luas)
